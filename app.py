847eafade1e5ede89f2942cb1f6d63d2281f3e82
SiriusKoan <k.peihsun@gmail.com>
2020-10-01 15:18:53 +0800

    finish forecast table

--- app.py
+++ app.py
@@ -10,10 +10,16 @@ from importlib import import_module
 app = Flask(__name__)
 CORS(app)
 
+
 @app.before_first_request
 def fetch_data():
     global data_cache
-    data_cache = {'forecast_update_time': time.time(), 'now_update_time': time.time() ,'forecast': update_forecast(), 'now': update_now()}
+    data_cache = {
+        "forecast_update_time": time.time(),
+        "now_update_time": time.time(),
+        "forecast": update_forecast(),
+        "now": update_now(),
+    }
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -21,10 +27,15 @@ def index():
     if request.method == "GET":
         return render_template("index.html", site=None, weather=None, color=None)
     if request.method == "POST":
-        site = request.form.get('site')
-        weather = json.loads(requests.post('%s/api'%config.host, json={'sitename': site, 'data_type': 'now'}).text)
-        return render_template("index.html", site=site, weather=weather, IMPORT=import_module)
-        
+        site = request.form.get("site")
+        weather = json.loads(
+            requests.post(
+                "%s/api" % config.host, json={"sitename": site, "data_type": "now"}
+            ).text
+        )
+        return render_template(
+            "index.html", site=site, weather=weather, IMPORT=import_module
+        )
 
 
 @app.route("/forecast", methods=["GET", "POST"])
@@ -33,30 +44,34 @@ def forecast():
         return render_template("forecast.html", site=None)
     if request.method == "POST":
         site = request.form.get("site")
-        weather = json.loads(requests.post('%s/api'%config.host, json = {'sitename': site, 'data_type': 'forecast'}).text)
-        return render_template("forecast.html", weather=weather, time=[weather['weatherElement'][0]['time'][0]['startTime'], weather['weatherElement'][0]['time'][-1]['endTime']])
+        weather = json.loads(
+            requests.post(
+                "%s/api" % config.host, json={"sitename": site, "data_type": "forecast"}
+            ).text
+        )
+        print(weather["weatherElement"][1]["time"])
+        return render_template("forecast.html", weather=weather, IMPORT=import_module)
 
 
 @app.route("/api", methods=["POST"])
 def api():
     payload = request.get_json()
-    sitename = payload['sitename']
-    data_type = payload['data_type']
-    if data_type == 'forecast':
-        if time.time() - data_cache['forecast_update_time'] > 3600:
+    sitename = payload["sitename"]
+    data_type = payload["data_type"]
+    if data_type == "forecast":
+        if time.time() - data_cache["forecast_update_time"] > 3600:
             # update every one hour
-            data_cache['forecast'] = update_forecast()
-        data = data_cache['forecast']
+            data_cache["forecast"] = update_forecast()
+        data = data_cache["forecast"]
         for site in data:
-            if site['locationName'] == sitename:
-                return str(site).replace('\'', '\"') # return json
-    if data_type == 'now':
-        if time.time() - data_cache['now_update_time'] > 3600:
-            data_cache['now'] = update_now()
-        data = data_cache['now']
+            if site["locationName"] == sitename:
+                return str(site).replace("'", '"')  # return json
+    if data_type == "now":
+        if time.time() - data_cache["now_update_time"] > 3600:
+            data_cache["now"] = update_now()
+        data = data_cache["now"]
         print(data)
-        return str(data[config.district_to_site[sitename]]).replace('\'', '\"')
-
+        return str(data[config.district_to_site[sitename]]).replace("'", '"')
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -5,15 +5,15 @@ import config
 
 def update_forecast():
     r = requests.get(
-        "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-061?Authorization=%s"%config.CWB_TOKEN
+        "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-061?"
+        "elementName=PoP6h,Wx,AT,T,RH,CI,WS,WD,Td"
+        "&Authorization=%s" % config.CWB_TOKEN
     )
-    data = json.loads(r.text)['records']['locations'][0]['location']
+    data = json.loads(r.text)["records"]["locations"][0]["location"]
     return data
 
+
 def update_now():
-    # TEMP, HUMD, H_24R, WDIR stand for temperature, relative humidity, accumulated precipitation in 24 hr, wind direction
-    # reference: https://opendata.cwb.gov.tw/opendatadoc/DIV2/A0001-001.pdf
-    # reference: https://opendata.cwb.gov.tw/opendatadoc/DIV2/A0003-001.pdf
     # sites list: https://e-service.cwb.gov.tw/wdps/obs/state.htm
 
     # get data from 自動氣象站(O-A0001-001)
@@ -21,31 +21,31 @@ def update_now():
         "https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001?"
         "Authorization=%s"
         "&locationName=石牌,松山,士林,內湖,大安,文山"
-        "&elementName=TEMP,HUMD,H_24R,WDIR"%config.CWB_TOKEN
+        "&elementName=TEMP,HUMD,H_24R,WDIR" % config.CWB_TOKEN
     )
     data1 = dict()
-    for site in json.loads(r1.text)['records']['location']:
-        data1[site['locationName']] = site['weatherElement']
+    for site in json.loads(r1.text)["records"]["location"]:
+        data1[site["locationName"]] = site["weatherElement"]
 
     # get data from 局屬氣象站(O-A0003-001)
     r2 = requests.get(
         "https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?"
         "Authorization=%s"
         "&locationName=臺北"
-        "&elementName=TEMP,HUMD,24R,WDIR"%config.CWB_TOKEN
+        "&elementName=TEMP,HUMD,24R,WDIR" % config.CWB_TOKEN
     )
     data2 = dict()
-    for site in json.loads(r2.text)['records']['location']:
-        data2[site['locationName']] = site['weatherElement']
+    for site in json.loads(r2.text)["records"]["location"]:
+        data2[site["locationName"]] = site["weatherElement"]
 
     # get 信義 data because there are two sites named '信義'
     r3 = requests.get(
         "https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001?"
         "Authorization=%s"
         "&locationName=信義"
-        "&elementName=TEMP,HUMD,H_24R,WDIR"%config.CWB_TOKEN
+        "&elementName=TEMP,HUMD,H_24R,WDIR" % config.CWB_TOKEN
     )
-    site = json.loads(r3.text)['records']['location'][1]
-    data3 = {site['locationName']: site['weatherElement']}
-    
+    site = json.loads(r3.text)["records"]["location"][1]
+    data3 = {site["locationName"]: site["weatherElement"]}
+
     return {**data1, **data2, **data3}
